f436d863c4af32c168104f371ad998922d441992
Victor Pantelemon <Victor.Pantelemon@centric.eu>
2021-07-03 18:23:07 +0300

    Add project files.

---
+++ CodeFirst.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeFirst.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ CodeFirst.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeFirst.Services
+{
+    public class Class1
+    {
+    }
+}
